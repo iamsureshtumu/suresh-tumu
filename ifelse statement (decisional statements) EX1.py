c3467745dccd976ad2efe142855b94f569a8f99d
suresh tumu <sureshtumu3691@gmail.com>
2019-07-02 20:32:11 +0530

    Add files via upload

---
+++ ifelse statement (decisional statements) EX1.py
@@ -0,0 +1,13 @@
+print("class is happeming")
+print("announcement for blood donation")
+interested="yes"
+if interested=="yes":
+    print("got to camp")
+    print("donate blood")
+    print("take some rest")
+    print("have some food")
+else:
+    print("go out")
+    print("have some food")
+print("classes will be resumed")
+
